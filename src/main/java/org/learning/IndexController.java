732fdd6a64058c301867dac536adfd25f1efcd11
Kinkey <george.sandu.grigorescu@gmail.com>
2016-04-25 22:36:03 +0300

    updated .gitingore to include windows specific files, java specific files, maven specific files. added a basic index controller and an index page. updated app.properties with basic postgres info.

---
+++ src/main/java/org/learning/IndexController.java
@@ -0,0 +1,20 @@
+package org.learning;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by kinks on 4/25/2016.
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String stuff(Model model){
+        model.addAttribute("name", "SomeName");
+        return "index";
+    }
+}
