3e50f2cd71b127d0b067a7f916ef7f8bc8732133
alexsch86 <schiaucualex@gmail.com>
2016-06-05 22:23:49 +0300

    Keep configuration only in sources configuration file and fix AspectJ dependencies for runtime

--- src/test/java/org/learning/caristuff/InfrastructureIntegrationTest.java
+++ src/test/java/org/learning/caristuff/InfrastructureIntegrationTest.java
@@ -5,8 +5,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.runner.RunWith;
-import org.learning.caristuff.config.InfrastructureTestConfig;
 import org.learning.caristuff.config.ConfigTestEnvironment;
+import org.learning.caristuff.infrastructure.config.InfrastructureConfig;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -15,7 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.joda.time.DateTimeUtils.setCurrentMillisSystem;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = {InfrastructureTestConfig.class, ConfigTestEnvironment.class})
+@SpringApplicationConfiguration(classes = {InfrastructureConfig.class, ConfigTestEnvironment.class})
 @Transactional
 @Ignore
 public class InfrastructureIntegrationTest extends AbstractJUnit4SpringContextTests {
